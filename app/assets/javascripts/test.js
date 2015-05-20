bb84877c3728017846f919b37ccf7c9b85befc67
Kevin Chow <k__chow@hotmail.com>
2015-05-20 18:10:41 -0400

    jQuery menu & picture

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,3 +1,16 @@
 jQuery(document).ready(function() {
 	jQuery('#tabs').tabs();
+	jQuery('.expand').click(function() {
+		$expand = $(this);
+
+		$info = $expand.next();
+		$text = $('p.expand').text();
+		$info.slideToggle(200, function() {
+			
+			$expand.text(function() {
+				
+			});
+
+		});
+	});
 });
