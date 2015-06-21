ec1a875ff510928fce60c898541acb37234ed9f0
Kevin Chow <k__chow@hotmail.com>
2015-06-20 20:46:55 -0400

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require bootstrap.min
+//= require jquery
 //= require jquery_ujs
 //= require test.js
 //= require smoothscroll.js
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,7 +74,7 @@ class UsersController < ApplicationController
   private
 
   	def user_params
-  		params.require(:user).permit(:name, :email, :password, :password_confirmation, :lat, :long)
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
 
     def logged_in_user
