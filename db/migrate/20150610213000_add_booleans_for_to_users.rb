23fe2057d142d9f4c65830b344ed707b056dbf43
Kevin Chow <k__chow@hotmail.com>
2015-06-25 14:43:44 -0400

    DB Fix

--- db/migrate/20150610213000_add_booleans_for_to_users.rb
+++ db/migrate/20150610213000_add_booleans_for_to_users.rb
@@ -2,6 +2,5 @@ class AddBooleansForToUsers < ActiveRecord::Migration
   def change
     add_column :users, :student, :boolean
     add_column :users, :tutor, :boolean
-    change_column :users, :both, :boolean, :default => :student && :tutor
-  end
+    add_column :users, :both, :boolean
 end
